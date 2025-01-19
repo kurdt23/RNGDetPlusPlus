cc7fcf141d43ca761b0ad94c9e9c477ecf7b90de
Mariia Kholodenko <148371058+kurdt23@users.noreply.github.com>
2025-01-19 06:23:18 +0500

    Update build_image.sh

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,1 +1,3 @@
-docker build -t zhxu_1.8.0-cuda11.1-cudnn8_py3.8 .
+#!/bin/sh
+# docker build -t zhxu_1.8.0-cuda11.1-cudnn8_py3.8 .
+docker build  --security-opt seccomp=unconfined --network=host -t zhxu_1.8.0-cuda11.1-cudnn8_py3.8 .
